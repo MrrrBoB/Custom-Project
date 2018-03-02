828f11a0ab5d7e8fbaa03c8928de0e9ed52e9291
Unknown <nathoward22@gmail.com>
2018-03-02 09:53:03 -0700

    Added Initial Objects

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	private Rigidbody2D rigid;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigid.AddForce (new Vector2 (Input.GetAxis("Horizontal")*speed, 0) , ForceMode2D.Force);
+		
+	}
+}
