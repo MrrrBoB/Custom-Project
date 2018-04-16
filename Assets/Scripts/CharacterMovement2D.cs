6d3d2b464bb87d6ab23aa632cfef56665935aa0b
Unknown <nathoward22@gmail.com>
2018-04-16 09:56:44 -0600

    Adjusted physics, need to work on shield

--- Assets/Scripts/CharacterMovement2D.cs
+++ Assets/Scripts/CharacterMovement2D.cs
@@ -11,11 +11,14 @@ public class CharacterMovement2D : MonoBehaviour {
 	private Vector3 moveDirection = Vector3.zero;
 	private Animator KnightAnimator;
 	private Attack_Melee attack;
+	public Transform GroundCheck;
+	private Rigidbody2D rigid;
 
 	// Use this for initialization
 	void Awake() {
 		KnightAnimator =GetComponent<Animator> ();
 		attack = GetComponent<Attack_Melee> ();
+		rigid = GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
@@ -32,30 +35,33 @@ public class CharacterMovement2D : MonoBehaviour {
 				facing = -1;
 			else if (axisX > 0)
 				facing = 1;
+			}
 
-		} 
 		else if (Input.GetAxisRaw ("Vertical") == 1)
 			facing = 0;
 		else
 			KnightAnimator.SetBool ("Moving", false);
-
+		
 		KnightAnimator.SetInteger ("FacingD", facing);
-		//Moving the controller
-		if (isGrounded()) {
-			moveDirection = new Vector3(Input.GetAxisRaw ("Horizontal"), 0);
-			moveDirection = transform.TransformDirection (moveDirection);
-			moveDirection *= speed/10;
-			if (Input.GetButton ("Jump"))
-				moveDirection.y = jumpSpeed/10;
-		}else
-		moveDirection.y -= gravity * Time.deltaTime;
-		transform.Translate(moveDirection);
 
+		//Moving the controller
+
+			moveDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0);
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed / 10;
+		transform.Translate (moveDirection);
 		//attack-refers to attack mele
 		if (Input.GetButtonDown("Fire1")) {
 			attack.Swipe (facing);
 
 		}
+	}
+	public void FixedUpdate()
+	{
+		
+		if (Input.GetButton ("Jump")&&isGrounded())
+				rigid.AddForce (new Vector2 (0, jumpSpeed*100), ForceMode2D.Force);
+		
 	}
 
 	private bool isGrounded()
@@ -63,10 +69,8 @@ public class CharacterMovement2D : MonoBehaviour {
 		Vector2 position = transform.position;
 		Vector2 direction = Vector2.down;
 		float distance = 2.25f;
-		Debug.DrawRay(position, direction, Color.green);
 		RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
 		if (hit.collider != null) {
-			Debug.Log ("TRUE");
 			return true;
 
 		}
---
+++ Assets/Scripts/Pit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pit : MonoBehaviour {
+	public GameManager manager;
+	// Use this for initialization
+	void Start () {
+		manager = FindObjectOfType<GameManager>().GetComponent<GameManager> ();
+	}
+
+	// Update is called once per frame
+	private void OnTriggerEnter2D ()
+	{
+		Debug.Log("fell in pit");
+
+	}
+}
